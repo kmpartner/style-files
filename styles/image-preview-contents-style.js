074f02e052db00a33961ad162ba0a3dc8a6f0446
Kaoru Masuyama <kmpartner@outlook.com>
2022-06-14 18:15:07 +0900

    change js file

--- styles/image-preview-contents-style.js
+++ styles/image-preview-contents-style.js
@@ -1,6 +1,6 @@
 const styleContents = {
-  ".bodyContainer": { marginTop: "2rem" },
-  ".body300x65Container": {
+  "bodyContainer": { marginTop: "2rem" },
+  "body300x65Container": {
     width: "300px",
     height: "65px",
     border: "0.5px solid gray",
@@ -8,27 +8,27 @@ const styleContents = {
     justifyContent: "space-between",
     marginBottom: "1rem"
   },
-  ".body300x65Text": { width: "235px", height: "100%", overflow: "hidden" },
-  ".body300x65TextContents": {
+  "body300x65Text": { width: "235px", height: "100%", overflow: "hidden" },
+  "body300x65TextContents": {
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
     padding: "5px 0 0 10px"
   },
-  ".body300x65TextTitle": {
+  "body300x65TextTitle": {
     fontSize: "16px",
     fontWeight: "bold",
     textAlign: "center"
   },
-  ".body300x65TextDescription": {
+  "body300x65TextDescription": {
     fontSize: "12px",
     fontWeight: "bold",
     paddingTop: "2px"
   },
-  ".body300x65Image": { width: "65px", height: "65px", objectFit: "cover" },
-  ".body300x300Container": { width: "300px", marginBottom: "1rem" },
-  ".body300x300Image": { width: "300px", height: "300px", objectFit: "cover" },
-  ".body300x300TextContainer": {
+  "body300x65Image": { width: "65px", height: "65px", objectFit: "cover" },
+  "body300x300Container": { width: "300px", marginBottom: "1rem" },
+  "body300x300Image": { width: "300px", height: "300px", objectFit: "cover" },
+  "body300x300TextContainer": {
     width: "300px",
     height: "60px",
     display: "flex",
@@ -38,17 +38,17 @@ const styleContents = {
     padding: "2px 10px 2px 10px",
     overflow: "hidden"
   },
-  ".body300x300TextTitle": {
+  "body300x300TextTitle": {
     fontSize: "18px",
     fontWeight: "bold",
     textAlign: "center"
   },
-  ".body300x300TextDescription": {
+  "body300x300TextDescription": {
     fontSize: "13px",
     fontWeight: "bold",
     paddingTop: "4px"
   },
-  ".bodyInPostContainer": {
+  "bodyInPostContainer": {
     width: "100%",
     maxWidth: "40rem",
     height: "150px",
@@ -56,28 +56,28 @@ const styleContents = {
     borderRadius: "6px",
     overflow: "hidden"
   },
-  ".bodyInPostContents": { display: "flex" },
-  ".bodyInPostImageContainer": {
+  "bodyInPostContents": { display: "flex" },
+  "bodyInPostImageContainer": {
     width: "150px",
     height: "150px",
     objectFit: "cover"
   },
-  ".bodyInPostTextContainer": { flexGrow: 2 },
-  ".bodyInPostTextTitle": {
+  "bodyInPostTextContainer": { flexGrow: 2 },
+  "bodyInPostTextTitle": {
     fontSize: "16px",
     fontWeight: "bold",
     textAlign: "center",
     padding: "20px 0 10px 0"
   },
-  ".bodyInPostTextDescription": { fontSize: "16px", padding: "0 5px" },
+  "bodyInPostTextDescription": { fontSize: "16px", padding: "0 5px" },
   "@media (min-width: 480px)": {
-    ".bodyInPostTextTitle": {
+    "bodyInPostTextTitle": {
       fontSize: "26px",
       fontWeight: "bold",
       textAlign: "center",
       padding: "20px 0 10px 0"
     },
-    ".bodyInPostTextDescription": { fontSize: "22px", padding: "0 20px" }
+    "bodyInPostTextDescription": { fontSize: "22px", padding: "0 20px" }
   }
 }
 
